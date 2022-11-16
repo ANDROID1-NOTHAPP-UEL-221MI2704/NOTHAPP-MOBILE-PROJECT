91b48823f60827377950ef4cee16572b97151633
longphanquangminh <lordaizensousuke2001@gmail.com>
2022-11-16 22:53:00 +0700

    first commit

---
+++ app/src/main/java/com/noth/nothapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.noth.nothapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.noth.nothapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
