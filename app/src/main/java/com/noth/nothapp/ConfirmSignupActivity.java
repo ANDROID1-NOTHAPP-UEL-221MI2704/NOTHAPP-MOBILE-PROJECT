eca8da0b057f4031739fb99decb0e54c466db34a
mytamnguyen <myytamm2002@gmail.com>
2022-12-02 23:53:53 +0700

    - Giao diện giỏ hàng - Giao diện đăng ký, xác thực đăng ký - Giao diện các item phổ biến, giỏ hàng, giảm giá, danh mục - Chỉnh sửa lại icon size, font size, button cho đồng nhất

---
+++ app/src/main/java/com/noth/nothapp/ConfirmSignupActivity.java
@@ -0,0 +1,14 @@
+package com.noth.nothapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_signup);
+    }
+}
