61d982649ae77c64021f4e5c0d457dfab2cb6318
duongtrungquan <vatlyhoc989@gmail.com>
2022-12-03 11:07:13 +0700

    Model and Util

---
+++ app/src/main/java/com/noth/nothapp/Utils/Util.java
@@ -0,0 +1,10 @@
+package com.noth.nothapp.Utils;
+
+import com.noth.nothapp.Model.Cart;
+
+import java.util.ArrayList;
+
+public class Util {
+    public static ArrayList<Cart> gioHangArrayList = new ArrayList<>();
+}
+
