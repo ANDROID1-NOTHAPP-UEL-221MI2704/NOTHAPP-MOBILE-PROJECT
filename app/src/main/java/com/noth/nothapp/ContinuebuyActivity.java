e0dd217aa32e56b2920b8cb98aa1dec6ababb1c6
Long Phan <111166256+longphanquangminh@users.noreply.github.com>
2022-12-06 11:03:16 +0700

    edit activity

--- app/src/main/java/com/noth/nothapp/ContinuebuyActivity.java
+++ app/src/main/java/com/noth/nothapp/ContinuebuyActivity.java
@@ -20,19 +20,12 @@ public class ContinuebuyActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         Intent intent = getIntent();
         int slMuaHang = intent.getIntExtra("soluong",0);
-        binding.txtSoLuongDonHang.setText(String.valueOf(slMuaHang));
         binding.btnTiepTucMuaHang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Xóa sản phẩm đã ấn mua thành công, sau đó khởi tạo Màn hình Cảm ơn và Tiếp tục mua hàng
                 Util.CartArrayList.clear();
                 startActivity(new Intent(ContinuebuyActivity.this, MainActivity.class));
-            }
-        });
-        binding.imgBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
             }
         });
         binding.chat.setOnClickListener(new View.OnClickListener() {
