58912a0bf2550a0b70e4793d29f21e085d1761c0
nguyenphu1205 <phunvl18411c@st.uel.edu.vn>
2022-12-06 18:07:44 +0700

    update AddressBookActivity

--- app/src/main/java/com/noth/nothapp/EditUserActivity.java
+++ app/src/main/java/com/noth/nothapp/EditUserActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ResetPassAcitivity extends AppCompatActivity {
+public class EditUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset_pass_acitivity);
+        setContentView(R.layout.activity_edit_user);
     }
 }
