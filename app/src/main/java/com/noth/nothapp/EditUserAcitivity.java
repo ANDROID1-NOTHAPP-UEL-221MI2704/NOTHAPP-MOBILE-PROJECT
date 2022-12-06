9824d708daba77936c2a8b20f9d6acd65a26f385
Hoàng Trần Ngọc Mai <116930698+hoangtranngocmai@users.noreply.github.com>
2022-12-06 16:28:58 +0700

    updating screen edituser and resetpass

---
+++ app/src/main/java/com/noth/nothapp/EditUserAcitivity.java
@@ -0,0 +1,14 @@
+package com.noth.nothapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditUserAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_acitivity);
+    }
+}
---
+++ app/src/main/java/com/noth/nothapp/ResetPassAcitivity.java
@@ -0,0 +1,14 @@
+package com.noth.nothapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPassAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pass_acitivity);
+    }
+}
