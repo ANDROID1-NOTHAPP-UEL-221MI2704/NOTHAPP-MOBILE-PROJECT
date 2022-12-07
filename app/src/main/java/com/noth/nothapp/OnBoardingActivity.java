35848bf64745bb905c53043acb2908c64ca288cd
Long Phan <111166256+longphanquangminh@users.noreply.github.com>
2022-12-07 23:40:32 +0700

    done chuyển màn hình slash sang onboarding và sang signin

--- app/src/main/java/com/noth/nothapp/OnBoardingActivity.java
+++ app/src/main/java/com/noth/nothapp/OnBoardingActivity.java
@@ -2,13 +2,28 @@ package com.noth.nothapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.noth.nothapp.databinding.ActivityDetailsBinding;
+import com.noth.nothapp.databinding.ActivityOnBoardingBinding;
 
 public class OnBoardingActivity extends AppCompatActivity {
-
+    ActivityOnBoardingBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_on_boarding);
+        binding = ActivityOnBoardingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnBatDau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OnBoardingActivity.this, SigninActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/noth/nothapp/SlashScreenActivity.java
+++ app/src/main/java/com/noth/nothapp/SlashScreenActivity.java
@@ -2,13 +2,30 @@ package com.noth.nothapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.noth.nothapp.databinding.ActivityOnBoardingBinding;
+import com.noth.nothapp.databinding.ActivitySlashScreenBinding;
 
 public class SlashScreenActivity extends AppCompatActivity {
-
+    ActivitySlashScreenBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_slash_screen);
+        binding = ActivitySlashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        LinearLayout llayout = (LinearLayout) findViewById(R.id.ll_Slash);
+        llayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SlashScreenActivity.this, OnBoardingActivity.class));
+                finish();
+            }
+        });
     }
 }
