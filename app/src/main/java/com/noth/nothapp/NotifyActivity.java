361e141c70d553a517031b71e13627c9666d1344
Long Phan <111166256+longphanquangminh@users.noreply.github.com>
2022-12-07 11:11:38 +0700

    update changes

--- app/src/main/java/com/noth/nothapp/NotifyActivity.java
+++ app/src/main/java/com/noth/nothapp/NotifyActivity.java
@@ -2,13 +2,25 @@ package com.noth.nothapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.noth.nothapp.databinding.ActivityNotifyBinding;
 
 public class NotifyActivity extends AppCompatActivity {
-
+    ActivityNotifyBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notify);
+        binding = ActivityNotifyBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NotifyActivity.this, MainActivity.class));
+            }
+        });
     }
 }
